d85cef944c22c0e237456a916f6dd6861f588f6c
Sam Gutmanis <18smg12@queensu.ca>
2023-03-23 13:24:58 -0400

    Updated Data Generator

--- DataGenerato.R
+++ DataGenerato.R
@@ -3,32 +3,11 @@ library("readr")
 Data=read_csv("Abundance_Data.csv")
 str(Data)
 
-# pH Stress
-pH_3=subset(Data,treatment_id=="C3")
-pH_5=subset(Data,treatment_id=="C5")
-pH_7=subset(Data,treatment_id=="C7")
-pH_8=subset(Data,treatment_id=="C8")
+#subset the full dataset to include only the treatments that we are analyzing
+CombinedData<-Data[Data$treatment_id == "C3"|Data$treatment_id =="C5"|
+                     Data$treatment_id =="C7"|Data$treatment_id =="C8"|
+                     Data$treatment_id =="CD18"|Data$treatment_id =="CD23"|
+                     Data$treatment_id =="HS37"|Data$treatment_id =="HS42"|
+                     Data$treatment_id =="HS48"|Data$treatment_id =="NC"|
+                     Data$treatment_id =="KC"|Data$treatment_id =="UT",]
 
-
-# Temperature Stress
-"18C"=subset(Data, treatment_id=="CD18")
-"23C"=subset(Data, treatment_id=="CD23")
-"37C"=subset(Data, treatment_id=="HS37")
-"42C"=subset(Data, treatment_id=="HS42")
-"48C"=subset(Data, treatment_id=="HS48")
-
-# Osmolarity Stress
-NaCl=subset(Data, treatment_id=="NC")
-KCl=subset(Data, treatment_id=="KC")
-
-# Control
-
-UT=subset(Data, treatment_id=="UT")
-
-
-pHDat=rbind(pH_3, pH_5, pH_7, pH_8, UT)
-TempDat=rbind("18C", "23C", "37C", "42C", "48C", UT)
-OsmoDat=rbind(NaCl, KCl, UT)
-write.csv(pHDat, file="pHDat.csv")
-write.csv(TempDat, file="TempDat.csv")
-write.csv(OsmoDat, file="OsmoDat.csv")
