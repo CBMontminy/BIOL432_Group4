064a6e438630aef642a498a6a8ef22b25f76941c
Sam Gutmanis <18smg12@queensu.ca>
2023-03-28 19:25:55 -0400

    DataGenerator Fix

--- DataGenerato.R
+++ DataGenerato.R
@@ -4,15 +4,14 @@ Data=read_csv("Abundance_Data.csv")
 str(Data)
 
 #subset the full dataset to include only the treatments that we are analyzing
-CombinedData<-Data[Data$treatment_id == "C3"|Data$treatment_id =="C5"|
-                     Data$treatment_id =="C7"|Data$treatment_id =="C8"|
-                     Data$treatment_id =="CD18"|Data$treatment_id =="CD23"|
-                     Data$treatment_id =="HS37"|Data$treatment_id =="HS42"|
-                     Data$treatment_id =="HS48"|Data$treatment_id =="NC"|
-                     Data$treatment_id =="KC"|Data$treatment_id =="UT",]
+CombinedData<-Data[Data$treatment_id == "C3"|Data$treatment_id =="C8"|
+                     Data$treatment_id =="CS23"| Data$treatment_id =="HS42"|
+                     Data$treatment_id =="NC"|Data$treatment_id =="UT",]
 
 dim(CombinedData)
 
 CombinedData<-na.omit(CombinedData)
 
 dim(CombinedData)
+
+write.csv(CombinedData, 'CombinedData.csv')
